ff20b4b1ced849626484f81240a979391e4e80a2
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-08 23:59:11 -0300

    Anotações de código

--- Curso_em_vídeo/aula2.java
+++ Curso_em_vídeo/aula2.java
@@ -1,3 +1,11 @@
+package Curso_em_vídeo;
+
 public class aula2 {
-    
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul"
+        c1.carga = "100"
+        c1.modelo = "MontBlanc"
+        
+    }
 }
