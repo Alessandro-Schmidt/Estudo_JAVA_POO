75d322798d90a8596a9d5710e5d26ae798fe5844
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-09 00:08:55 -0300

    Aprendendo a imprimir resultados
    Através do comando "System.out.prinlin();", conseguimos printar uma frase.
    Através do comando this, adicionamos a variável que gostaríamos de adicionar na frase, que foi declarada no objeto c1.

--- Curso_em_vídeo/Aula2/Caneta.java
+++ Curso_em_vídeo/Aula2/Caneta.java
@@ -6,7 +6,10 @@ public class Caneta {
     int carga;
     boolean tampada;
     void status(){
+        System.out.println("\nEsta é a caneta");
+        System.out.println("Seu modelo: " + this.modelo);
         System.out.println("Uma caneta "+ this.cor);
+        System.out.println("Está tampada? "+this.tampada);
     }
 
     void rabiscar(){
