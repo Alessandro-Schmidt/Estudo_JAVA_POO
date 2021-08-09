665133cb2d2ae7a73635a273ee7ec9c3429bcba8
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-09 00:18:27 -0300

    Definição de dois novos métodos;
    Método tampar e destampar foram declarados;

--- Curso_em_vídeo/Aula2/Caneta.java
+++ Curso_em_vídeo/Aula2/Caneta.java
@@ -12,8 +12,10 @@ public class Caneta {
         System.out.println("Seu modelo: " + this.modelo);
         System.out.println("Uma caneta "+ this.cor);
         System.out.println("Está tampada? "+this.tampada);
+        System.out.println("A dela está em "+this.carga+"%");
+        System.out.println("<<<<<<<<<<<");
         /* 
-        Aqui declaramos uma funcionalidade, cujo objetivo é printar o status da caneta
+        Aqui declaramos uma |Método| (funcionalidade), cujo objetivo é printar o status da caneta
         */
 
     }
@@ -22,10 +24,11 @@ public class Caneta {
 
     }
     void tampar(){
+        this.tampada = true;
 
     }
     void destampar(){
-
+        this.tampada = false;
     }
 
 
