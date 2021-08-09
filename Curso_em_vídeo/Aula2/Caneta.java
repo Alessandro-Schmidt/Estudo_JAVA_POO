9035c06922edac21666dee62a9f345f883e7416b
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-09 00:04:06 -0300

    Alterações nas pastas.

--- Curso_em_vídeo/Aula2/Caneta.java
+++ Curso_em_vídeo/Aula2/Caneta.java
@@ -1,9 +1,13 @@
+package Aula2;
 public class Caneta {
     String modelo; 
     String cor; 
     float ponta; 
     int carga;
     boolean tampada;
+    void status(){
+        System.out.println("Uma caneta "+ this.cor);
+    }
 
     void rabiscar(){
 
---
+++ Curso_em_vídeo/Aula2/aula2.java
@@ -0,0 +1,12 @@
+package Aula2;
+
+public class aula2 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.carga = 100;
+        c1.modelo = "MontBlanc";
+        c1.tampada = false;
+        c1.status();
+    }
+}
