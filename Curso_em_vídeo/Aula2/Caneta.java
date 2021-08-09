996e73edc3f54fded07358cd2f7285237734a0aa
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-09 00:14:04 -0300

    Comentários.
    Código está comentado;

--- Curso_em_vídeo/Aula2/Caneta.java
+++ Curso_em_vídeo/Aula2/Caneta.java
@@ -1,15 +1,21 @@
 package Aula2;
 public class Caneta {
+    /* Aqui declaramos os atributos do molde caneta*/
     String modelo; 
     String cor; 
     float ponta; 
     int carga;
     boolean tampada;
-    void status(){
+
+    void status(){ 
         System.out.println("\nEsta é a caneta");
         System.out.println("Seu modelo: " + this.modelo);
         System.out.println("Uma caneta "+ this.cor);
         System.out.println("Está tampada? "+this.tampada);
+        /* 
+        Aqui declaramos uma funcionalidade, cujo objetivo é printar o status da caneta
+        */
+
     }
 
     void rabiscar(){
--- Curso_em_vídeo/Aula2/aula2.java
+++ Curso_em_vídeo/Aula2/aula2.java
@@ -2,11 +2,11 @@ package Aula2;
 
 public class aula2 {
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
+        Caneta c1 = new Caneta(); /*Aqui criamos uma nova caneta de nome c1.*/
         c1.cor = "Azul";
         c1.carga = 100;
         c1.modelo = "MontBlanc";
         c1.tampada = false;
-        c1.status();
+        c1.status(); // Aqui chamamos o comando status, que printa todo o status da caneta.
     }
 }
