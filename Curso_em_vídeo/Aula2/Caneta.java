5fd9c955c0145fcd10e18987fa7f58d0c7354807
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-09 00:36:35 -0300

    Desenvolvimento de conceitos
    Desenvolvimento de conceitos;

--- Curso_em_vídeo/Aula2/Caneta.java
+++ Curso_em_vídeo/Aula2/Caneta.java
@@ -6,13 +6,15 @@ public class Caneta {
     float ponta; 
     int carga;
     boolean tampada;
+    int valor; 
 
     void status(){ 
-        System.out.println("\nEsta é a caneta");
+        System.out.println("\nEsta é a caneta ");
         System.out.println("Seu modelo: " + this.modelo);
+        System.out.println("Seu preço: R$ "+this.valor);
         System.out.println("Uma caneta "+ this.cor);
         System.out.println("Está tampada? "+this.tampada);
-        System.out.println("A dela está em "+this.carga+"%");
+        System.out.println("A carga dela está em "+this.carga+"%");
         System.out.println("<<<<<<<<<<<");
         /* 
         Aqui declaramos uma |Método| (funcionalidade), cujo objetivo é printar o status da caneta
@@ -20,14 +22,22 @@ public class Caneta {
 
     }
 
-    void rabiscar(){
-
+    void rabiscar(){ // outro método
+        if (this.tampada ==true){ // IMPORTANTE: ESTRUTURA IF E ELSE;
+            System.out.println("Eita! Eu não consigo rabiscar com a caneta tampada!");
+        } else{
+            System.out.println("Rabisquei uma coisa no meu caderno!");
+        }
+        
     }
     void tampar(){
-        this.tampada = true;
+        this.tampada = true; // This significa o objeto que foi chamado, pois o comando ficará: Referência ao objeto que o chamou; 
+        // Objeto c1.tampar()
+        //Sacaste? 
 
     }
     void destampar(){
+        System.out.println("Destampei! Agora posso usar.");
         this.tampada = false;
     }
 
--- Curso_em_vídeo/Aula2/aula2.java
+++ Curso_em_vídeo/Aula2/aula2.java
@@ -6,7 +6,27 @@ public class aula2 {
         c1.cor = "Azul";
         c1.carga = 100;
         c1.modelo = "MontBlanc";
-        c1.tampada = false;
-        c1.status(); // Aqui chamamos o comando status, que printa todo o status da caneta.
+        c1.valor = 2;
+        //c1.tampada = false;
+        c1.tampar();
+        //c1.status(); // Aqui chamamos o comando status, que printa todo o status da caneta.
+        //c1.rabiscar(); // Dá erro, pois tampamos a caneta lá em cima;
+        //c1.destampar(); // Destampamos ela aqui;
+        //c1.status(); // Pedimos o status dela, vemos que está destampada;
+        //c1.rabiscar(); // E agora o rabiscar funciona
+
+        Caneta Shakespierre = new Caneta();
+        Shakespierre.modelo="500 anos de William Shakespeare";
+        Shakespierre.cor="Vermelha com azul, cores da bandeira Britânica";
+        Shakespierre.carga=100;
+        Shakespierre.valor=10000;
+        Shakespierre.carga = 100;
+        Shakespierre.tampar();
+        Shakespierre.status();
+        Shakespierre.rabiscar();
+        Shakespierre.destampar();
+        Shakespierre.rabiscar();
+        
+
     }
 }
