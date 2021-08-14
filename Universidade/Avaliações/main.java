e232507b778a3d3ac08a355297b88c0f8d3682ec
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-13 23:29:54 -0300

    Mudanças
    Agora foi!

---
+++ Universidade/Avaliações/main.java
@@ -0,0 +1,10 @@
+/*
+autor: Alessandro Maciel Schmidt
+*/
+package Avaliações;
+
+public class main {
+    public static void main(String[] args) { //Usei a abreviação psvm
+        System.out.println("Hello World!"); //usei a abreviação sout
+    }
+}
