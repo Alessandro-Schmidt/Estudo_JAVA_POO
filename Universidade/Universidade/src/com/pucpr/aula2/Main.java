daecde2a9461110daab4aa51fcf928b385490ed8
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-14 00:10:40 -0300

    ignore

--- Universidade/Universidade/src/com/pucpr/aula2/Main.java
+++ Universidade/Universidade/src/com/pucpr/aula2/Main.java
@@ -1,10 +1,14 @@
 // Developed by Alessandro Maciel Schmidt
 package com.pucpr.aula2;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// Exercício 1
+	// Exercício 1 - Crie um programa que o usuário entre com dois números e diga qual o maior;
+        System.out.println("Digite o primeiro número: ");
+        Scanner
 
     }
 }
