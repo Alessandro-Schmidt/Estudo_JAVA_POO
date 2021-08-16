1110555d84025440419eda12b947d9e217499957
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-15 22:12:47 -0300

    Comecei.
    Sumário do exercício 2 ainda incompleto.

---
+++ Curso_em_vídeo/Aula2/pessoal_aula2.java
@@ -0,0 +1,8 @@
+package Aula2;
+
+public class pessoal_aula2 {
+    public static void main(String[] args) {
+        // Farei a declaração de 2 objetos que pensei. Acordar cedo
+        // e
+    }
+}
