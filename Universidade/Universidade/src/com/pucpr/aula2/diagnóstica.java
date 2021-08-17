44b0090797ac20f10d49ccc5e12e15d0bd2ed2f6
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-17 10:17:48 -0300

    Update diagnóstica.java

--- Universidade/Universidade/src/com/pucpr/aula2/diagnóstica.java
+++ Universidade/Universidade/src/com/pucpr/aula2/diagnóstica.java
@@ -2,7 +2,7 @@
 package com.pucpr.aula2;
 import java.util.InputMismatchException;
 import java.util.Scanner;
-public class Main {
+public class diagnóstica {
     public static void main(String[] args) {
 	// Exercício 1 - Crie um programa que o usuário entre com dois números e diga qual o maior;
         /*
