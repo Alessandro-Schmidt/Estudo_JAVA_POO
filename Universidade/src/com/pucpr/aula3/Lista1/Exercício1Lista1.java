9272dd42047ab63194dffb96fdd3027156d2a603
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-20 10:50:16 -0300

    AAAAAAAAAAAAAAAAAAA

--- Universidade/src/com/pucpr/aula3/Lista1/Exercício1Lista1.java
+++ Universidade/src/com/pucpr/aula3/Lista1/Exercício1Lista1.java
@@ -1,7 +1,11 @@
 package src.com.pucpr.aula3.Lista1;
 
+import java.util.Scanner;
+
 public class Exercício1Lista1 {
     public static void main(String[] args) {
-        
+        int num1, num2, soma;
+        Scanner scanner =
+        for (int i = num1; i<=)
     }
 }
