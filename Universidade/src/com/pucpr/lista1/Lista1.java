f071e8d1dcdb98d1023852d1d6f33d5e5e3742fa
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-24 15:49:13 -0300

    Vários exerc'\icios resolvidos

--- Universidade/src/com/pucpr/lista1/Lista1.java
+++ Universidade/src/com/pucpr/lista1/Lista1.java
@@ -3,14 +3,17 @@ package src.com.pucpr.lista1;
 import java.util.Scanner;
 
 public class Lista1 {
-    public void exercício1(){
+    Scanner scanner = new Scanner(System.in);
+    // A linha acima faz com que todos os métodos tenham acesso ao scanner, sem a necessidade de declará-lo todas as
+    // vezes. Essa variável é um atributo de classe.
+    public void exercicio1(){
          /*
         Faça um programa que receba dois números. Calcule e mostre:
         1. A soma dos números ímpares desse intervalo de números, incluindo os números digitados;
         2. A multiplicação dos números pares desse intervalo, incluindo os digitados.
         * */
         int num1, num2, soma=0,multiplicação=1;
-        Scanner scanner = new Scanner(System.in);
+
         do {
             System.out.print("Digite o primeiro número: ");
             num1 = scanner.nextInt();
@@ -30,7 +33,43 @@ public class Lista1 {
         System.out.println("\nA soma dos números ímpares é: "+soma+"\nA multiplicação dos números pares deste intervalo é: "+multiplicação);
     }
     public void exercicio2(){
-        
+        /*
+        Escreva um programa completo que permita a qualquer aluno introduzir, pelo teclado, uma sequência arbitrária de
+        notas e que mostra na tela, como resultado, a correspondente média aritmética. O número de notas deve ser indeterminado,
+        e o calculo só parará quando for introduzido um número negativo;
+         */
+        float soma = 0;
+        int contador = 0;
+        while (true){
+            System.out.print("Digite uma nota (valor negativo para sair): ");
+            float num = scanner.nextFloat();
+            if (num<0.0f){
+                break;
+            }
+            soma+=num;
+            contador+=1;
+        }
+        System.out.print("A média das notas digitadas é: "+ (soma/contador));
+    }
+    public void exercicio3(){
+        // faça um programa que calcule a soma de  todos os numeros naturais, abaixo de 1000, que sejam múltiplos de 3
+        // ou 5
+        int soma = 0;
+        for (int i =0; i<1000;i++){
+            if (i%3==0 || i%5==0 ){
+                // aquele || ali em cima significa or
+                soma+=i;
+            }
+        }
+        System.out.print("A soma é: "+soma);
+    }
+    public void exercicio4(){
+        while (true){
+            System.out.print();
+        }
+    }
+    public void exercicio5(){
+
     }
 
 }
--- Universidade/src/com/pucpr/lista1/Main.java
+++ Universidade/src/com/pucpr/lista1/Main.java
@@ -1,9 +1,44 @@
 package src.com.pucpr.lista1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Lista1 lista1 = new Lista1();
-        //lista1.exercício1();
+        while (true){
+            System.out.print("\nMENU DA LISTA 1!\n");
+            System.out.println("[1] - Exercício 1;");
+            System.out.println("[2] - Exercício 2;");
+            System.out.println("[3] - Exercício 3;");
+            System.out.println("[4] - Exercício 4;");
+            System.out.println("[5] - Exercício 5;");
+            System.out.println("[6] - Sair;");
+            System.out.print("Sua opção: ");
+            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
+            switch (option){
+                case 1:
+                    lista1.exercicio1();
+                    break;
+                case 2:
+                    lista1.exercicio2();
+                    break;
+                case 3:
+                    lista1.exercicio3();
+                    break;
+                case 4:
+                    lista1.exercicio4();
+                    break;
+                case 5:
+                    lista1.exercicio5();
+                    break;
+                case 6:
+                    return;
+            }
+        }
+        //lista1.exercicio1();
+        //lista1.exercicio2();
+        //lista1.exercicio3();
 
     }
 }
