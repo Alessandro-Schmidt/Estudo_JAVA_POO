0a3d754f0537be18454ef67b2f22596cc1880375
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-24 15:04:36 -0300

    Exercício 1 correção

--- Universidade/src/com/pucpr/lista1/Lista1.java
+++ Universidade/src/com/pucpr/lista1/Lista1.java
@@ -11,10 +11,12 @@ public class Lista1 {
         * */
         int num1, num2, soma=0,multiplicação=1;
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite o primeiro número: ");
-        num1 = scanner.nextInt();
-        System.out.print("Digite o segundo número: ");
-        num2 = scanner.nextInt();
+        do {
+            System.out.print("Digite o primeiro número: ");
+            num1 = scanner.nextInt();
+            System.out.print("Digite o segundo número: ");
+            num2 = scanner.nextInt();
+        }while (num1>num2);
         for (int i = num1; i<=num2; i++){
             System.out.print(".."+i);
         }
@@ -28,7 +30,7 @@ public class Lista1 {
         System.out.println("\nA soma dos números ímpares é: "+soma+"\nA multiplicação dos números pares deste intervalo é: "+multiplicação);
     }
     public void exercicio2(){
-
+        
     }
 
 }
--- Universidade/src/com/pucpr/lista1/Main.java
+++ Universidade/src/com/pucpr/lista1/Main.java
@@ -3,6 +3,7 @@ package src.com.pucpr.lista1;
 public class Main {
     public static void main(String[] args) {
         Lista1 lista1 = new Lista1();
-        lista1.exercício1();
+        //lista1.exercício1();
+
     }
 }
