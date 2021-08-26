5c09329c77f3992409d38979f8ea7d3dcee46b15
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-08-26 18:28:14 -0300

    Eu nem acredito que funciona

---
+++ Universidade/src/com/pucpr/lista2/Main.java
@@ -0,0 +1,10 @@
+package src.com.pucpr.lista2;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        pessoa Alessandro = new pessoa();
+        Alessandro.status();
+        Alessandro.setaltura();
+        Alessandro.status();
+    }
+}
