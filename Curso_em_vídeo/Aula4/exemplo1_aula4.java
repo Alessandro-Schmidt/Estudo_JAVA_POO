45e8294a0e962abccb45e3961f9d88488b97d70b
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-05 02:07:03 -0300

    Usando o método main como usado na universidade

--- Curso_em_vídeo/Aula4/exemplo1_aula4.java
+++ Curso_em_vídeo/Aula4/exemplo1_aula4.java
@@ -1,16 +1,18 @@
 package Aula4;
 
 import java.util.Scanner;
-
 public class exemplo1_aula4 {
-    public static void main(String[] args) {
-        /*Celular iPhone = new Celular();
+
+    public static void exemplo01(){
+        Celular iPhone = new Celular();
         iPhone.status();
         iPhone.setNumero(1298734);
         iPhone.setModelo("SchmidtPhone");
         iPhone.status();
         System.out.printf("\nEste modelo (%s) possui um chip com o número: %d\n",iPhone.getModelo(),iPhone.getNumero());
-        */
+    }
+
+    public static void exemplo02(){
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o número do seu Telefone: ");
         int num = input.nextInt();
@@ -18,6 +20,12 @@ public class exemplo1_aula4 {
         String modelo = input.next();
         Celular Zen = new Celular(num, modelo);
         Zen.status();
+    }
 
+
+    public static void main(String[] args) {
+        exemplo01();
+        exemplo02();
     }
 }
+
