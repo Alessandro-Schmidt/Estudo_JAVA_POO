a7bd3907f9a96519ec725330fcaaf643cfd247bc
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-05 02:02:19 -0300

    Exemplos com inputs realizados

--- Curso_em_vídeo/Aula4/exemplo1_aula4.java
+++ Curso_em_vídeo/Aula4/exemplo1_aula4.java
@@ -4,16 +4,20 @@ import java.util.Scanner;
 
 public class exemplo1_aula4 {
     public static void main(String[] args) {
-        Celular iPhone = new Celular();
+        /*Celular iPhone = new Celular();
         iPhone.status();
         iPhone.setNumero(1298734);
         iPhone.setModelo("SchmidtPhone");
         iPhone.status();
         System.out.printf("\nEste modelo (%s) possui um chip com o número: %d\n",iPhone.getModelo(),iPhone.getNumero());
-
-        Celular Zen = new Celular();
-        Scanner input = new Scanner();
-        ;
+        */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o número do seu Telefone: ");
+        int num = input.nextInt();
+        System.out.println("Digite o modelo do Telefone: ");
+        String modelo = input.next();
+        Celular Zen = new Celular(num, modelo);
+        Zen.status();
 
     }
 }
