3d6f745fcd5ce875492e9a8097be31cdf86ab764
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-05 01:57:05 -0300

    Update exemplo1_aula4.java

--- Curso_em_vídeo/Aula4/exemplo1_aula4.java
+++ Curso_em_vídeo/Aula4/exemplo1_aula4.java
@@ -1,5 +1,7 @@
 package Aula4;
 
+import java.util.Scanner;
+
 public class exemplo1_aula4 {
     public static void main(String[] args) {
         Celular iPhone = new Celular();
@@ -8,5 +10,10 @@ public class exemplo1_aula4 {
         iPhone.setModelo("SchmidtPhone");
         iPhone.status();
         System.out.printf("\nEste modelo (%s) possui um chip com o número: %d\n",iPhone.getModelo(),iPhone.getNumero());
+
+        Celular Zen = new Celular();
+        Scanner input = new Scanner();
+        ;
+
     }
 }
