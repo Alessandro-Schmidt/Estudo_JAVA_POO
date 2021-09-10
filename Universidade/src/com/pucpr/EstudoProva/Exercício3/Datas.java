c6a769a6da5e985c9cc486b16d897a00e96729b5
Alessandro M. Schmidt <alessandromacielschmidt@hotmail.com>
2021-09-09 23:35:55 -0300

    Dúvidas na 4

---
+++ Universidade/src/com/pucpr/EstudoProva/Exercício3/Datas.java
@@ -0,0 +1,42 @@
+package src.com.pucpr.EstudoProva.Exercício3;
+
+import java.util.Date;
+
+public class Datas {
+    private int dias;
+    private int mês;
+    private int ano;
+
+    // Constructors:
+
+    public Datas() {
+        System.out.println(data);
+    }
+
+
+    // Métodos getters e setters
+
+    public int getDias() {
+        return dias;
+    }
+
+    public void setDias(int dias) {
+        this.dias = dias;
+    }
+
+    public int getMês() {
+        return mês;
+    }
+
+    public void setMês(int mês) {
+        this.mês = mês;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+}
---
+++ Universidade/src/com/pucpr/EstudoProva/Exercício3/Main.java
@@ -0,0 +1,7 @@
+package src.com.pucpr.EstudoProva.Exercício3;
+
+public class Main {
+    public static void main(String[] args) {
+        Datas hoje = new Datas();
+    }
+}
